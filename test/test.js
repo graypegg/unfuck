48da62534288b7f702205331277d0fbed255cb2e
Graham Pegg (MacBook) <graham@citrusmedia.com>
2017-02-11 21:23:35 -0500

    Better tests for AST Conversion and source prep

--- test/test.js
+++ test/test.js
@@ -7,6 +7,20 @@ let uf       = require('../dist/unfuck.js');
 
 let assert  = require('assert');
 
+describe('Source Prepping', function() {
+  describe('Character Stripping', function() {
+    it('should return a string containing only valid brainfuck instructions', function() {
+      assert.equal(prepare({}, '+H+e-l-l,o,w.o.r[ld]!'), '++--,,..[]');
+    });
+  });
+
+  describe('Skipped-Loop Comment Pattern', function() {
+    it('should return a string without an inital loop', function() {
+      assert.equal(prepare({}, '[test.example]++--'), '++--');
+    });
+  });
+});
+
 describe('Basic Compilation', function() {
   describe('"+"', function() {
     it('should return a AST with one positive SFT action', function() {
@@ -54,19 +68,43 @@ describe('Basic Compilation', function() {
 describe('Optimisation', function() {
   describe('Collapsing', function() {
     it('should return a AST with one positive SFT action, adding 2 to the current cell', function() {
-      assert.deepEqual(optimise({}, analyse({}, '++')), [ { is: 'SFT', body: 2 } ]);
+      var ast = [{"is":"SFT","body":1},{"is":"SFT","body":1}];
+      assert.deepEqual(optimise({}, ast), [ { is: 'SFT', body: 2 } ]);
     });
   });
 
   describe('Nullifying', function() {
     it('should return a AST with no content', function() {
-      assert.deepEqual(optimise({}, analyse({}, '++-++---+-+++-+---')), [ ]);
+      var ast = [{"is":"SFT","body":1},{"is":"SFT","body":1},{"is":"SFT","body":-1},{"is":"SFT","body":1},{"is":"SFT","body":1},{"is":"SFT","body":-1},{"is":"SFT","body":-1},{"is":"SFT","body":-1},{"is":"SFT","body":1},{"is":"SFT","body":-1},{"is":"SFT","body":1},{"is":"SFT","body":1},{"is":"SFT","body":1},{"is":"SFT","body":-1},{"is":"SFT","body":1},{"is":"SFT","body":-1},{"is":"SFT","body":-1},{"is":"SFT","body":-1}];
+      assert.deepEqual(optimise({}, ast), [ ]);
     });
   });
 
   describe('Cell Clearing', function() {
     it('should return a AST with SFT+10 and SET=0', function() {
-      assert.deepEqual(optimise({}, analyse({}, '++++++++++[-]')), [ { is: 'SFT', body: 10 }, { is: 'SET', body: 0 } ]);
+      var ast = [{"is":"SFT","body":1},{"is":"SFT","body":1},{"is":"SFT","body":1},{"is":"SFT","body":1},{"is":"SFT","body":1},{"is":"SFT","body":1},{"is":"SFT","body":1},{"is":"SFT","body":1},{"is":"SFT","body":1},{"is":"SFT","body":1},{"is":"IF","body":[{"is":"SFT","body":-1}]}];
+      assert.deepEqual(optimise({}, ast), [ { is: 'SFT', body: 10 }, { is: 'SET', body: 0 } ]);
+    });
+  });
+
+  describe('Fusing', function() {
+    it('should return a AST with THREE RELSFT instructions, and ends with a MOV of the sum of steps taken', function() {
+      var ast = [{"is":"SFT","body":1},{"is":"SFT","body":1},{"is":"MOV","body":1},{"is":"MOV","body":1},{"is":"SFT","body":-1},{"is":"SFT","body":-1},{"is":"MOV","body":-1},{"is":"SFT","body":1},{"is":"OUT"}];
+      assert.deepEqual(optimise({}, ast), [{"is":"RELSFT","body":{"value":2,"move":0}},{"is":"RELSFT","body":{"value":-2,"move":2}},{"is":"RELSFT","body":{"value":1,"move":1}},{"is":"MOV","body":1},{"is":"OUT"}]);
+    });
+  });
+
+  describe('Multiplication', function() {
+    it('should return a AST with ONE MUL instruction, with two factors (x3 and x4)', function() {
+      var ast = [{"is":"SFT","body":1},{"is":"SFT","body":1},{"is":"IF","body":[{"is":"SFT","body":-1},{"is":"MOV","body":1},{"is":"SFT","body":1},{"is":"SFT","body":1},{"is":"SFT","body":1},{"is":"MOV","body":1},{"is":"SFT","body":1},{"is":"SFT","body":1},{"is":"SFT","body":1},{"is":"SFT","body":1},{"is":"MOV","body":-1},{"is":"MOV","body":-1}]}];
+      assert.deepEqual(optimise({}, ast), [{"is":"SFT","body":2},{"is":"MUL","body":{"factors":[{"move":1,"factor":3},{"move":2,"factor":4}]}}]);
+    });
+  });
+
+  describe('Alternate Multiplication', function() {
+    it('should return a AST with ONE MUL instruction, with two factors (x3 and x4)', function() {
+      var ast = [{"is":"SFT","body":1},{"is":"SFT","body":1},{"is":"IF","body":[{"is":"MOV","body":1},{"is":"SFT","body":1},{"is":"SFT","body":1},{"is":"SFT","body":1},{"is":"MOV","body":1},{"is":"SFT","body":1},{"is":"SFT","body":1},{"is":"SFT","body":1},{"is":"SFT","body":1},{"is":"MOV","body":-1},{"is":"MOV","body":-1},{"is":"SFT","body":-1}]}];
+      assert.deepEqual(optimise({}, ast), [{"is":"SFT","body":2},{"is":"MUL","body":{"factors":[{"move":1,"factor":3},{"move":2,"factor":4}]}}]);
     });
   });
 });
@@ -106,6 +144,100 @@ describe('Settings', function() {
       let bf  = '+++++++++++++++[>+++++++++++++++++<-]>+.[-].';
       let c = uf.compiler({ type: Array, out: Number });
       assert.deepEqual(c.run(bf, ''), [256, 0]);
+    });
+  });
+});
+
+describe('AST Conversion', function() {
+  describe('SFT', function() {
+    describe('Positive', function() {
+      it('should return an array containing a JS program that adds 2 to the current cell', function() {
+        var ast = [{"is":"SFT","body":2}];
+        assert.deepEqual(convert({ target: 'simple-es6' }, ast), ["t[p]+=2"]);
+      });
+    });
+
+    describe('Negative', function() {
+      it('should return an array containing a JS program that subtracts 2 from the current cell', function() {
+        var ast = [{"is":"SFT","body":-2}];
+        assert.deepEqual(convert({ target: 'simple-es6' }, ast), ["t[p]-=2"]);
+      });
+    });
+  });
+
+  describe('RELSFT', function() {
+    it('should return an array containing a JS program that adds 1 to the current cell and the next cell', function() {
+      var ast = [{"is":"RELSFT","body":{"value":1,"move":0}},{"is":"RELSFT","body":{"value":1,"move":1}}];
+      assert.deepEqual(convert({ target: 'simple-es6' }, ast), ["t[p]+=1","t[p+1]+=1"]);
+    });
+  });
+
+  describe('MOV', function() {
+    it('should return an array containing a JS program that moves the current cell pointer 2 cells to the right', function() {
+      var ast = [{"is":"MOV","body":2}];
+      assert.deepEqual(convert({ target: 'simple-es6' }, ast), ["p+=2"]);
+    });
+  });
+
+  describe('SET', function() {
+    it('should return an array containing a JS program that sets the current cell to 5', function() {
+      var ast = [{"is":"SET","body":5}];
+      assert.deepEqual(convert({ target: 'simple-es6' }, ast), ["t[p]=5"]);
+    });
+  });
+
+  describe('MUL', function() {
+    describe('Copying', function() {
+      it('should return an array containing a JS program that sets the next cell to the same value as the current cell', function() {
+        var ast = [{"is":"SFT","body":1},{"is":"MUL","body":{"factors":[{"move":1,"factor":1}]}}];
+        assert.deepEqual(convert({ target: 'simple-es6' }, ast), ["t[p]+=1","t[p+1]+=t[p]","t[p]=0"]);
+      });
+    });
+
+    describe('Multiplicative Factor', function() {
+      it('should return an array containing a JS program that sets the next cell to the result of 2*3', function() {
+        var ast = [{"is":"SFT","body":3},{"is":"MUL","body":{"factors":[{"move":1,"factor":2}]}}];
+        assert.deepEqual(convert({ target: 'simple-es6' }, ast), ["t[p]+=3","t[p+1]+=t[p]*2","t[p]=0"]);
+      });
+    });
+  });
+
+  describe('INP', function() {
+    describe('Simple ES6', function() {
+      it('should return an array containing a JS program that pushes the next item in the input stack onto the current cell', function() {
+        var ast = [{"is":"INP"}];
+        assert.deepEqual(convert({ target: 'simple-es6' }, ast), ["t[p]=(i.length<1?0:i.shift())"]);
+      });
+    });
+
+    describe('Interactive ES6', function() {
+      it('should return an array containing a JS program that pushes the result of the input function onto the current cell', function() {
+        var ast = [{"is":"INP"}];
+        assert.deepEqual(convert({ target: 'interactive-es6' }, ast), ["t[p]=i(t[p])"]);
+      });
+    });
+  });
+
+  describe('OUT', function() {
+    describe('Simple ES6', function() {
+      it('should return an array containing a JS program that pushes the current cell onto the output stack', function() {
+        var ast = [{"is":"OUT"}];
+        assert.deepEqual(convert({ target: 'simple-es6' }, ast), ["o.push(t[p])"]);
+      });
+    });
+
+    describe('Interactive ES6', function() {
+      it('should return an array containing a JS program that runs the output function with the current cell\'s data', function() {
+        var ast = [{"is":"OUT"}];
+        assert.deepEqual(convert({ target: 'interactive-es6' }, ast), ["o(t[p])"]);
+      });
+    });
+  });
+
+  describe('IF', function() {
+    it('should return an array containing a JS program that begins a while loop that will run once and shift the cell pointer one cell to the right', function() {
+      var ast = [{"is":"SFT","body":1},{"is":"IF","body":[{"is":"MOV","body":1}]}];
+      assert.deepEqual(convert({ target: 'simple-es6' }, ast), ["t[p]+=1","while(t[p]!=0){p+=1}"]);
     });
   });
 });
@@ -152,7 +284,7 @@ describe('Simple Programs', function() {
   });
 
   describe('Async I/O', function() {
-    it('should return an array containing the number 15 (from the addition of the input values 5 and 10)', function( done ) {
+    it('should return 5, the number inputted by the input lambda', function( done ) {
       let bf  = ',.';
       let c = uf.compiler({ target: 'interactive-es6', in: Number, out: Number });
 
